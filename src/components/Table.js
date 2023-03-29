aa9e2351a170b634e7f5d2169d9377b7a963195d
Lauren <halpertln@gmail.com>
2023-03-29 10:06:29 -0600

    extra deliverable 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,11 +5,16 @@ function Table({ plates = [], money, setMoney }) {
   const emptyPlates = plates.map((_, index) => (
     <div key={index} className="empty-plate" style={{ top: -7 * index }} />
   ));
+  function handleAddMoney() {
+    setMoney(()=> money + 10)
+  }
+
   return (
     <>
       <h1 className="remaining">
         You have: ${money} remaining!
       </h1>
+      <button onClick={handleAddMoney}>Add $10 to My Sushi Budget</button>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
       </div>
